6092e08c8a83e8e3e492b82f12eb38259f7b6049
Victor Gomenza <viguitars738@gmail.com>
2023-04-24 23:03:35 -0300

    Create Multiply.js

---
+++ Multiply.js
@@ -0,0 +1,12 @@
+/* This code does not execute properly. Try to figure out why. */
+
+function multiply(a, b) {
+	return a * b;
+}
+
+console.log(multiply());
+
+console.log(multiply(1, 1)); // 1
+console.log(multiply(2, 1)); // 2
+console.log(multiply(2, 2)); // 4
+console.log(multiply(3, 5)); // 15

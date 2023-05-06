e52f0e4caa58aa2db9bd8dea1c090a9da4552aa8
Victor Gomenza <viguitars738@gmail.com>
2023-05-06 02:42:06 -0300

    Update Chain me.js

--- Chain me.js
+++ Chain me.js
@@ -1,30 +1,28 @@
-/* cSpell: disable */
+/* Write a generic function chainer that takes a starting value, and an array of functions to execute on it (array of symbols for Ruby).
 
-/* Write a generic function chainer that takes a starting value, and an array of functions 
-to execute on it (array of symbols for Ruby).
-
-The input for each function is the output of the previous function (except the first 
-function, which takes the starting value as its input). Return the final value after 
-execution is complete. */
+The input for each function is the output of the previous function (except the 
+first function, which takes the starting value as its input). Return the final 
+value after execution is complete. */
 
 function add(num) {
-  return num + 1;
+	return num + 1;
 }
 
-function mult(num) {
-  return num * 30;
+function multiply(num) {
+	return num * 30;
 }
 
-chain(2, [add, mult]);
+// chain(2, [add, multiply]);
 // returns 90;
 
 function chain(input, fs) {
-  let output = input;
+	let output = input;
 
-  for (let func of fs) {
-    output = func(output);
-  }
+	for (let i = 0; i < fs.length; i += 1) {
+		output = fs[i](output);
+	}
 
-  return output;
+	return output;
 }
-console.log(chain(2, [add, mult]));
+
+console.log(chain(2, [add, multiply]));

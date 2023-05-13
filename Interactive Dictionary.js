738ac6f46cc48878b9736718b4b1f14702151318
Victor Gomenza <viguitars738@gmail.com>
2023-05-13 13:57:22 -0300

    Create Interactive Dictionary.js

---
+++ Interactive Dictionary.js
@@ -0,0 +1,42 @@
+/* cSpell:disable */
+/* In this kata, your job is to create a class Dictionary which you can add words to and their 
+entries. Example:
+
+const d = new Dictionary();
+
+d.newEntry('Apple', 'A fruit that grows on trees');
+
+console.log(d.look('Apple'));
+A fruit that grows on trees
+
+console.log(d.look('Banana'));
+
+Can't find entry for Banana
+Good luck and happy coding! */
+
+class Dictionary {
+	constructor() {
+		this.entries = {};
+	}
+
+	newEntry(key, value) {
+		this.entries[key] = value;
+	}
+
+	look(key) {
+		if (key in this.entries) {
+			return this.entries[key];
+		}
+
+		return `Can't find entry for ${key}`;
+	}
+}
+
+const d = new Dictionary();
+
+d.newEntry('Apple', 'A fruit that grows on trees');
+
+console.log(d.look('Apple'));
+// A fruit that grows on trees
+
+console.log(d.look('Banana'));

6b1d4a75a75b93025ff167625dce6cec34cc01ee
Victor Gomenza <viguitars738@gmail.com>
2023-05-17 12:19:35 -0300

    Update Chain me.js

--- Chain me.js
+++ Chain me.js
@@ -1,5 +1,6 @@
 /* cSpell:disable */
-/* Write a generic function chainer that takes a starting value, and an array of functions to execute on it (array of symbols for Ruby).
+/* Write a generic function chainer that takes a starting value, and an array 
+of functions to execute on it (array of symbols for Ruby).
 
 The input for each function is the output of the previous function (except the 
 first function, which takes the starting value as its input). Return the final 

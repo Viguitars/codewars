617570766c607deb7b46bdf0fbfe63471791b1d3
Victor Gomenza <viguitars738@gmail.com>
2023-05-17 10:28:13 -0300

    Update RGB To Hex Conversion.js

--- RGB To Hex Conversion.js
+++ RGB To Hex Conversion.js
@@ -14,21 +14,18 @@ rgb(255, 255, 300) // returns FFFFFF
 rgb(0,0,0) // returns 000000
 rgb(148, 0, 211) // returns 9400D3 */
 
-// decimal = primera cifra hexadecimal × 16 + segunda cifra hexadecimal
-// a=10, b=11, c=12, d=13, e=14 y f=15.
-
 function rgb(r, g, b) {
-  const HEX_DIGITS = "0123456789ABCDEF";
-  const channels = [r, g, b];
-  let convertedHex = [];
+	const HEX_DIGITS = '0123456789ABCDEF';
+	const channels = [r, g, b];
+	const convertedHex = [];
 
-  for (let i = 0; i < channels.length; i++) {
-    let value = Math.max(0, Math.min(255, channels[i]));
-    let aux1 = Math.floor(value / 16);
-    let aux2 = value % 16;
-    convertedHex.push(aux1, aux2);
-  }
-  return convertedHex.map((item) => HEX_DIGITS[item]).join("");
+	for (let i = 0; i < channels.length; i += 1) {
+		const value = Math.max(0, Math.min(255, channels[i]));
+		const aux1 = Math.floor(value / 16);
+		const aux2 = value % 16;
+		convertedHex.push(aux1, aux2);
+	}
+	return convertedHex.map(item => HEX_DIGITS[item]).join('');
 }
 
 console.log(rgb(0, 0, 0)); // 000000

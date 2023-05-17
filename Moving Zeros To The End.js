6dc48eb4213f58623ff7accab18a62dd03cda265
Victor Gomenza <viguitars738@gmail.com>
2023-05-17 12:19:42 -0300

    Update Moving Zeros To The End.js

--- Moving Zeros To The End.js
+++ Moving Zeros To The End.js
@@ -1,5 +1,5 @@
-/* Write an algorithm that takes an array and moves all of the zeros to the end, preserving the 
-order of the other elements. */
+/* Write an algorithm that takes an array and moves all of the zeros to the 
+end, preserving the order of the other elements. */
 
 function moveZeros(arr) {
 	let zerosInArr = 0;

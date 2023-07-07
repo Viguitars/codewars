7add035d937a0d444851e4ec0e35fe51a20abe53
Victor Gomenza <viguitars738@gmail.com>
2023-07-06 21:38:08 -0300

    Update Mad Max Recursion Road.js

--- Mad Max Recursion Road.js
+++ Mad Max Recursion Road.js
@@ -14,24 +14,24 @@ Rules:
 reduce */
 
 function max(array) {
-    // Verifica si el array está vacío. Si es así, devuelve -Infinity.
-    if (array.length === 0) {
-      return -Infinity;
-    }
-    // Si el array tiene un solo elemento, devuelve ese elemento.
-    if (array.length === 1) {
-      return array[0];
-    }
-    // Divide el array en dos mitades aproximadamente.
-    const firstHalfMax = max(array.slice(0, array.length / 2));
-    const secondHalfMax = max(array.slice(array.length / 2, array.length));
-    /* Llama recursivamente a la función max en cada mitad del array.
+	// Verifica si el array está vacío. Si es así, devuelve -Infinity.
+	if (array.length === 0) {
+		return -Infinity;
+	}
+	// Si el array tiene un solo elemento, devuelve ese elemento.
+	if (array.length === 1) {
+		return array[0];
+	}
+	// Divide el array en dos mitades aproximadamente.
+	const firstHalfMax = max(array.slice(0, array.length / 2));
+	const secondHalfMax = max(array.slice(array.length / 2, array.length));
+	/* Llama recursivamente a la función max en cada mitad del array.
     Compara los resultados de las dos llamadas recursivas y devuelve el
     máximo de ambos. */
-    return firstHalfMax > secondHalfMax ? firstHalfMax : secondHalfMax;
-  }
-  
-  console.log(max([1, 2, 3, 4, 5])); // 5
-  console.log(max([5])); // 5
-  console.log(max([])); // -Infinity
-  console.log(max([99, 2, 100, 4, 5])); // 100
+	return firstHalfMax > secondHalfMax ? firstHalfMax : secondHalfMax;
+}
+
+console.log(max([1, 2, 3, 4, 5])); // 5
+console.log(max([5])); // 5
+console.log(max([])); // -Infinity
+console.log(max([99, 2, 100, 4, 5])); // 100
